1846f4cc4e5dd644758317b9206854495079a244
Boris <seikenfr@yahoo.fr>
2021-10-08 16:09:40 +0200

    added: server.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
+// const express = require('express');
 const express = require('express');
-
 const app = express();
+
+let bodyParser = require('body-parser')
+
+
+bodyParser.urlencoded({ extended: false })
+bodyParser.json()
+app.use(express.json())
+
+app.use('/contacts', require('./routes/contact'))
 
 app.get('/', (req, res)=>{
   res.send('hello server')
 })
 app.listen(8000, ()=>{
-  console.log('🤖  → Connecté sur le port 8000 📮');
+  console.log(' ϟ tibidit ϟ ● 🤖 → Connecté sur le port 8000 📮');
 })
