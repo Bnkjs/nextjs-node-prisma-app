fb281e3c66a03cef0d49a9d5585fbbf903362365
Boris <seikenfr@yahoo.fr>
2021-10-08 18:01:49 +0200

    added: form to app container

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Card } from '../src/card/card'
 import { PrismaClient } from '@prisma/client'
 import { v4 as uuidv4 } from "uuid"
-
-
+import { Form } from '../src/form/ AddUserContactForm'
 export default function Home({contacts}){
 
   console.log(contacts);
   return (
     <div className={styles.container}>
+      <Form method='POST'/>
       {
         contacts.map((c)=>{
           return <Card key={uuidv4()} name={c.name} email={c.email} />
